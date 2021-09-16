c7db021155d38aa626c59ac9804de8a435bd4ed6
kodebae <heykarmendurbin@gmail.com>
2021-09-16 13:13:40 -0400

    Jumping on clouds solution addded

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Hacker-Rank-Interview-Prep
+
+Created for study purposes. Saving so I can review my work again later. 
--- warm-up.js
+++ warm-up.js
@@ -1,9 +1,10 @@
-/** Sales By Match
+/* * 
+! Sales By Match
  * There is a large pile of socks that must be paired by color. 
  * Given an array of integers representing the color of each sock, 
  * determine how many pairs of socks with matching colors there are.
- * int n: the number of socks in the pile
- * int ar[n]: the colors of each sock
+ ? int n: the number of socks in the pile
+ ? int ar[n]: the colors of each sock
  * **/
 
  function sockMerchant(n, ar) {
@@ -18,3 +19,32 @@
             } 
             return total_socks; // returning the final count
     } 
+
+/* * 
+ ! Jumping on the Clouds
+ * There is a new mobile game that starts with consecutively numbered clouds. 
+ * Some of the clouds are thunderheads and others are cumulus. 
+ * The player can jump on any cumulus cloud having a number that is equal to the number of the current cloud plus  or . 
+ * The player must avoid the thunderheads. 
+ * Determine the minimum number of jumps it will take to jump from the starting postion to the last cloud. 
+ * It is always possible to win the game.
+ * For each game, you will get an array of clouds numbered  if they are safe or  if they must be avoided.
+ ? jumpingOnClouds has the following parameter(s):
+ ? int c[n]: an array of binary integers
+ ? int: the minimum number of jumps required
+ ? Print the minimum number of jumps needed to win the game.
+ * */
+
+ function jumpingOnClouds(c) {
+    let jumps = 0; // a place to store jumps
+    for(let i = 0; i < c.length; i++) { // looping over c 
+        if(c[i + 2] === 0) { // if we can move 2
+            jumps++; // add to jumps
+            i++; // go to the next [i]
+        } else if(c[i + 1] === 0) { // if we can move 1
+            jumps++; // add to jumps
+        }
+    }
+    return jumps; // return the final count
+}
+
